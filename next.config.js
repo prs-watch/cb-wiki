826094d71af9c4d3283b096c4344374c1fad230f
prs-watch <scarsdaleny15@yahoo.co.jp>
2022-01-02 09:15:43 +0900

    ~fix deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+// prefix設定
+const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : ''
+
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
+  trailingSlash: true,
 };
